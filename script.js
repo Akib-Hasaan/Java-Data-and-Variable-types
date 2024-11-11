13964ee00a525f86bcc552595caa642a23e7d362
Akib-Hasaan <akibhasan582@gmail.com>
2024-11-11 21:37:35 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+// (1) The 2 types of data
+
+// String (All non-numerical values)
+let text = "ABC-xyz"
+console.log(typeof text);
+//*NOTE* = Use "" for non-numerical values
+
+//Number (All numerical values)
+let numericalValue = 100
+console.log(typeof numericalValue);
+//*NOTE* = DON'T use "" for numerical values
+
+
+// (2) All 3 variable types
+// const name = "Akib Hasan"
+// let kothaShuru = "Amar nam holo"
+// var kothaShesh = "ar amar boyosh 17"
+
+// Simple use of all 3 variable and data types at once
+// console.log(kothaShuru+" "+name+" "+kothaShesh);
